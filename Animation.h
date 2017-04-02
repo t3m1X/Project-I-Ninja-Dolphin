7adcf5d1495581da0c81fe32b30205de37835dcc
Sergi PR <sergiparrar@gmail.com>
2017-04-02 21:10:55 +0200

    Animation class rehaul

--- Animation.h
+++ Animation.h
@@ -2,37 +2,82 @@
 #define __ANIMATION_H__
 
 #include "SDL/include/SDL_rect.h"
-#define MAX_FRAMES 25
+#define MAX_SPRITES 25
 
 class Animation
 {
 public:
 	bool loop = true;
 	float speed = 1.0f;
-	SDL_Rect frames[MAX_FRAMES];
+	SDL_Rect frames[MAX_SPRITES];
 
 private:
 	float current_frame;
-	int last_frame = 0;
 	int loops = 0;
+
+	int animation_size = 0;
+	int* animation = nullptr;
 
 public:
 
-	void PushBack(const SDL_Rect& rect)
+	bool SetUp(int first_x, int first_y, int sprite_w, int sprite_h, int columns, int total_sprites, 
+		char* animation_sequence)
 	{
-		frames[last_frame++] = rect;
+		if (total_sprites > MAX_SPRITES)
+			return false;
+
+		if (animation_sequence != "")
+			++animation_size;
+
+		for (char* tmp = animation_sequence; *tmp != '\0'; ++tmp) {
+			if (*tmp == ',') ++animation_size;
+		}
+
+		if (animation != nullptr)
+			delete[] animation;
+
+		animation = new int[animation_size];
+
+		animation[0] = 0; //initialize first value
+		for (int i = 0; i < animation_size; ++animation_sequence) {
+			if (*animation_sequence == ',') {
+				++i;
+				animation[i] = 0;
+			}
+			else {
+				animation[i] *= 10;
+				animation[i] += *animation_sequence - '0';
+			}
+		}
+
+		frames[0].x = first_x; frames[0].y = first_y;
+		frames[0].w = sprite_w; frames[0].h = sprite_h;
+
+		for (int i = 1; i < total_sprites; ++i) {
+			frames[i].x = frames[i - 1].x + sprite_w;
+			if (frames[i].x > columns * sprite_w) {
+				frames[i].x = 0;
+				frames[i].y = frames[i - 1].y + sprite_h;
+			}
+		}
+
+		return true;
+	}
+
+	~Animation() {
+		delete[] animation;
 	}
 
 	SDL_Rect& GetCurrentFrame()
 	{
 		current_frame += speed;
-		if (current_frame >= last_frame)
+		if (current_frame >= animation_size)
 		{
-			current_frame = (loop) ? 0.0f : last_frame - 1;
+			current_frame = (loop) ? 0.0f : animation_size - 1;
 			loops++;
 		}
 
-		return frames[(int)current_frame];
+		return frames[animation[(int)current_frame]];
 	}
 
 	bool Finished() const

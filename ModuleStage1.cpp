3d8b32eecb6006186d2647e741920f8ea11ce6b9
Sergi PR <sergiparrar@gmail.com>
2017-04-06 02:49:41 +0200

    Rendering two layers of background

--- ModuleStage1.cpp
+++ ModuleStage1.cpp
@@ -15,7 +15,7 @@ ModuleStage1::~ModuleStage1() {
 
 bool ModuleStage1::Start() {
 	bool ret = true;
-	stage_background = App->textures->Load("revamp_spritesheets/lvl1_placeholder - copia.png");
+	stage_background = App->textures->Load("revamp_spritesheets/lvl1_placeholder.png");
 	music = App->audio->LoadMusic("music/rough_and_tumble.ogg");
 	water_texture = App->textures->Load("revamp_spritesheets/base_water_animation.png");
 
@@ -32,12 +32,14 @@ bool ModuleStage1::Start() {
 	SDL_Rect background_rect;
 	SDL_QueryTexture(stage_background, nullptr, nullptr, &background_rect.w, &background_rect.h);
 	background_pos = -background_rect.h  + SCREEN_HEIGHT; //The multiplier is due to the size of the sprite currently
-
 	return ret;
 }
 
 update_status ModuleStage1::Update() {
-	App->render->Blit(stage_background, 0, background_pos);
+	SDL_Rect background = { 0,0,704,6532 };
+	App->render->Blit(stage_background, 0, background_pos, 1, &background);
+	background.x += background.w;
+	App->render->Blit(stage_background, 0, background_pos, 1, &background);
 	if (background_pos < 0)
 		background_pos += SCROLL_SPEED;
 

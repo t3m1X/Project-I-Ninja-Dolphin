a97ea4a3d9509488b373a4b769a90d002c9b4503
Sergi PR <sergiparrar@gmail.com>
2017-04-06 02:10:56 +0200

    Now title screen is at the center of the screen

--- ModuleStageIntro.cpp
+++ ModuleStageIntro.cpp
@@ -21,8 +21,8 @@ bool ModuleStageIntro::Start() {
 	music = App->audio->LoadMusic("music/fighting_thunder.ogg");
 	title_texture = App->textures->Load("revamp_spritesheets/attract_screen.png");
 
-	title_screen.SetUp(0, 0, 518, 592, 6, 11, "0,0,0,0,0,1,2,3,4,5,6,7,8,9,10");
-	title_screen.speed = 0.1f;
+	title_screen.SetUp(0, 0, 518, 592, 6, 11, "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10");
+	title_screen.speed = 0.15f;
 	
 	
 	App->audio->PlayMusic(music);
@@ -37,7 +37,7 @@ update_status ModuleStageIntro::Update() {
 	if (App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_REPEAT)
 		App->fade->FadeToBlack(this, App->stage1, 0.5f);
 
-	App->render->Blit(title_texture, 0, 0, 1, &title_screen.GetCurrentFrame());
+	App->render->Blit(title_texture, (SCREEN_WIDTH-title_screen.frames[0].w)/2, (SCREEN_HEIGHT - title_screen.frames[0].h) / 2, 1, &title_screen.GetCurrentFrame());
 
 	return UPDATE_CONTINUE;
 }
@@ -45,10 +45,8 @@ update_status ModuleStageIntro::Update() {
 bool ModuleStageIntro::CleanUp() {
 	bool ret = true;
 	App->audio->StopMusic();
-	//
 	App->intro->Disable();
-	//
-	//App->intro->title_screen.Finished();
+	title_screen.CleanUp();
 
 	return ret;
 }

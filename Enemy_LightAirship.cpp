9f5481e03f8a768c5a4229ef403e6a1798937f51
Sergi PR <sergiparrar@gmail.com>
2017-04-24 16:32:31 +0200

    Changed LightAirship's path

--- Enemy_LightAirship.cpp
+++ Enemy_LightAirship.cpp
@@ -7,10 +7,13 @@ Enemy_LightAirship::Enemy_LightAirship(int x, int y) : Enemy(x, y)
 	fly.SetUp(0, 0, 50, 67, 4, 4, "0,1,2,3");
 	fly.speed = 0.2f;
 
-	path.PushBack({ 0,5 }, 160, &fly);
+	path.PushBack({ 0, 3 }, 10, &fly);
+	path.PushBack({ 0, -2 }, 40, &fly);
 	path.PushBack({ 0,-5 }, 500, &fly);
 
 	collider = App->collision->AddCollider({ 0, 0, 50, 67 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
+
+	original_position = position;
 
 	original_y = y;
 }

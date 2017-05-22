b022c2dce2f1094e60cc552a6e7fa9e871ac9d33
Megaoski <oscar_2596@hotmail.com>
2017-05-22 12:18:56 +0200

    Box now seems correct

--- Enemy_Box.cpp
+++ Enemy_Box.cpp
@@ -7,16 +7,16 @@
 
 Enemy_Box::Enemy_Box(int x, int y) : Enemy(x, y)
 {
-	walk.SetUp(0, 181, 44, 68, 3, 3, "0,1,2");
-	walk.speed = 0.2f;
+	walk.SetUp(0, 689, 43, 42, 3, 3, "0,1,2");
+	walk.speed = 0.1f;
 
 	
 	
 
 	path.PushBack({ 0,0 }, 1000, &walk);
-	/*path.PushBack({ 0,-1.5f }, 300, &walk);*/
+	
 
-	collider = App->collision->AddCollider({ 0, 190, 45, 70 }, COLLIDER_TYPE::COLLIDER_ENEMY_GROUND, (Module*)App->enemies);
+	collider = App->collision->AddCollider({ 0, 190, 42, 40 }, COLLIDER_TYPE::COLLIDER_ENEMY_GROUND, (Module*)App->enemies);
 
 	original_position = position;
 

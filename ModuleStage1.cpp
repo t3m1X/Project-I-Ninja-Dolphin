8a76c5570cf8018254b4f860cf85279545249ffc
Sergi PR <t3m1X@users.noreply.github.com>
2017-06-04 11:45:53 +0200

    Changed position of the bosses

--- ModuleStage1.cpp
+++ ModuleStage1.cpp
@@ -167,8 +167,8 @@ bool ModuleStage1::Start() {
 	App->enemies->AddEnemy(ENEMY_TYPES::MOONAIRSHIP, 140, 2290);
 	App->enemies->AddEnemy(ENEMY_TYPES::MOONAIRSHIP, 520, 2290);
 
-	App->enemies->AddEnemy(ENEMY_TYPES::BOSS, 290, 700);
-	App->enemies->AddEnemy(ENEMY_TYPES::BOSS_2, 290, 30);
+	App->enemies->AddEnemy(ENEMY_TYPES::BOSS, 290, 545);
+	App->enemies->AddEnemy(ENEMY_TYPES::BOSS_2, 515, -14);
 
 	return ret;
 }
